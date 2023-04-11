63ad5981bfe2aec27f02ab2d6b7c02047ab61cfb
zgweks <92344996+zgwek@users.noreply.github.com>
2023-04-11 02:08:28 -0400

    output rows with js

--- chapter08/project1/js/ch08-proj01.js
+++ chapter08/project1/js/ch08-proj01.js
@@ -1,6 +1,11 @@
 
-const tax_rate = prompt('Enter tax rate (0.10)');
-const shipping_threshold = prompt('Enter shipping threshold (1000)');
+// const tax_rate = prompt('Enter tax rate (0.10)');
+// const shipping_threshold = prompt('Enter shipping threshold (1000)');
 
 /* add loop and other code here ... in this simple exercise we are not
    going to concern ourselves with minimizing globals, etc */
+
+   for(let i = 0; i < 3; i++){
+      
+      outputCartRow(i, 20)
+  }
--- chapter08/project1/js/functions.js
+++ chapter08/project1/js/functions.js
@@ -1,6 +1,27 @@
 /* define your functions here */
 
+function calculateTotal(quantity,price) { 
+    return quantity * price; 
+   }
 
+function outputCartRow(item, total) { 
+    
+    document.write("<tr>" + 
+            "<td>" + `<img src="images/${cart[item].product.filename}">`+ "<td>" +
+            cart[item].product.title + "<td>" + 
+             cart[item].quantity + "<td>" +  
+            cart[item].product.price + "<td>" +  
+            calculateTotal(cart[item].quantity,cart[item].product.price).toFixed(2) + "<td>" +  "</tr>"); 
+    }
+   
+    
+//     <tr>
+//     <td><img src="images/106060.jpg"></td>
+//     <td>View of Houses in Delft</td>
+//     <td>1</td>
+//     <td>$125.00</td>
+//     <td>$125.00</td>
+// </tr>
 
 
 
